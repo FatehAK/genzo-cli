de32e6e534f06888a07dd35aab9fc6253aa3fe7e
fa7ehak <fa7ehak@gmail.com>
2023-01-05 19:54:07 +0530

    feat: Execute git init based on inquirer query and support cli args

--- src/index.js
+++ src/index.js
@@ -1,107 +1,54 @@
 import os from 'os';
-import fs from 'fs-extra';
-import { resolve, join, sep } from 'pathe';
+import { join } from 'pathe';
 import ansi from 'ansi-colors';
-import fetch from 'node-fetch';
-import minimatch from 'minimatch';
+import minimist from 'minimist';
 import inquirer from 'inquirer';
 import { createSpinner } from 'nanospinner';
-import { escapeRegex } from './utils';
-import { GITHUB_DIR_TYPE, GITHUB_FILE_TYPE, CONFIG_FILE_NAME, GITHUB_PATH_REGEX, INQUIRER_DEFAULT_OPTS } from './constants';
+import { downloadDirectory, callGitHubApi, copyDirectory, getLocalTemplates, initializeGit } from './utils';
+import { CONFIG_FILE_NAME, GITHUB_PATH_REGEX, INQUIRER_DEFAULT_OPTS } from './constants';
 
 (async function () {
+  // get cli arguments
+  const argv = minimist(process.argv.slice(2), {
+    alias: {
+      help: 'h',
+      version: 'v',
+      git: 'g',
+      install: 'i',
+    },
+  });
+
+  // check if config file and template path exists
+  const homeDir = os.homedir();
   let config;
   try {
-    const homeDir = os.homedir();
     config = (await import(join(homeDir, CONFIG_FILE_NAME))).default;
   } catch {
-    console.log(ansi.red(`\nConfig file ${ansi.bold(CONFIG_FILE_NAME)} missing in home directory`));
+    console.log(ansi.red(`\nConfig file ${ansi.bold(CONFIG_FILE_NAME)} missing in home directory (${ansi.bold(homeDir)})`));
     process.exit(1);
   }
-
   if (!config.templatePath) {
     console.log(ansi.red(`\n${ansi.bold('templatePath')} missing in config file ${CONFIG_FILE_NAME}`));
     process.exit(1);
   }
 
-  // register inquirer fuzzy search plugin
-  inquirer.registerPrompt('search-list', (await import('inquirer-search-list')).default);
-
-  const { templatePath, githubToken, authorName: defaultAuthor, slotPaths = [], customSlots = {} } = config;
-  let authorName = defaultAuthor;
-  let repoName;
-
-  async function downloadDirectory(url, targetDir) {
-    // create a folder if not exists
-    await fs.ensureDir(targetDir);
-    targetDir = resolve(targetDir);
-
-    const contents = await callGitHubApi(githubToken, url);
-
-    for (const content of contents) {
-      if (content.type === GITHUB_FILE_TYPE) {
-        const fileStr = await callGitHubApi(githubToken, content.url, true);
-        const filePathAbs = join(targetDir, content.name);
-        // replace slots in the file and write it
-        fs.writeFile(filePathAbs, replaceSlots(filePathAbs, fileStr));
-      } else if (content.type === GITHUB_DIR_TYPE) {
-        await downloadDirectory(content.url, join(targetDir, content.name));
-      } else {
-        console.log(ansi.red(`\n Unknown type for ${ansi.bold(content.name)}`));
-      }
-    }
-  }
-
-  async function copyDirectory(path, targetDir) {
-    // create a folder if not exists
-    await fs.ensureDir(targetDir);
-    targetDir = resolve(targetDir);
-
-    const contents = await fs.readdir(path);
-
-    for (const content of contents) {
-      const contentAbsPath = path + sep + content;
-      if (fs.lstatSync(contentAbsPath).isFile()) {
-        const fileStr = await fs.readFile(contentAbsPath, 'utf8');
-        const filePathAbs = join(targetDir, content);
-        // replace slots in the file and write it
-        fs.writeFile(filePathAbs, replaceSlots(filePathAbs, fileStr));
-      } else if (fs.lstatSync(contentAbsPath).isDirectory()) {
-        await copyDirectory(contentAbsPath, join(targetDir, content));
-      } else {
-        console.log(ansi.red(`\n Unknown type for ${ansi.bold(content)}`));
-      }
-    }
-  }
-
-  function replaceSlots(filePath, content) {
-    if (slotPaths.some(pattern => minimatch(filePath, pattern))) {
-      const slots = {
-        '[REPO_NAME]': repoName.trim(),
-        '[AUTHOR_NAME]': authorName.trim(),
-        ...customSlots,
-      };
-      const regex = new RegExp(escapeRegex(Object.keys(slots).join('|')), 'g');
-      return content.replace(regex, match => slots[match]);
-    }
-    return content;
-  }
-
   try {
-    // check if template path is a github url or local folder
-    const isGithubPath = GITHUB_PATH_REGEX.test(templatePath);
+    // STEP 1 - check if template path is a github url or local folder
+    const isGithubPath = GITHUB_PATH_REGEX.test(config.templatePath);
 
     let templates;
     if (isGithubPath) {
       const spinner = createSpinner('Fetching templates from GitHub...').start();
-      templates = await callGitHubApi(githubToken, templatePath);
+      templates = await callGitHubApi(config.templatePath, config.githubToken);
       spinner.success({ text: 'Templates Fetched!\n' });
     } else {
       const spinner = createSpinner('Searching templates...\n').start();
-      templates = await getLocalTemplates(templatePath);
-      spinner.success({ text: `Templates located at ${ansi.cyan(templatePath)}\n` });
+      templates = await getLocalTemplates(config.templatePath);
+      spinner.success({ text: `Templates located at ${ansi.cyan(config.templatePath)}\n` });
     }
 
+    // STEP 2 - ask user to select a template and enter their repo name
+    inquirer.registerPrompt('search-list', (await import('inquirer-search-list')).default); // register inquirer fuzzy search plugin
     const { inputTemplate, repoNameInput } = await inquirer.prompt([
       {
         type: 'search-list',
@@ -121,10 +68,10 @@ import { GITHUB_DIR_TYPE, GITHUB_FILE_TYPE, CONFIG_FILE_NAME, GITHUB_PATH_REGEX,
         ...INQUIRER_DEFAULT_OPTS,
       },
     ]);
+    config.repoName = repoNameInput;
 
-    repoName = repoNameInput;
-
-    if (!authorName?.trim().length) {
+    // STEP 3 - ask user their authorName only if not already defined in .rc file
+    if (!config.authorName?.trim().length) {
       const { authorNameInput } = await inquirer.prompt({
         type: 'input',
         message: 'Enter author name',
@@ -135,30 +82,36 @@ import { GITHUB_DIR_TYPE, GITHUB_FILE_TYPE, CONFIG_FILE_NAME, GITHUB_PATH_REGEX,
         },
         ...INQUIRER_DEFAULT_OPTS,
       });
-      authorName = authorNameInput;
+      config.authorName = authorNameInput;
     }
 
+    // STEP 4 - Download or Copy the template into chosen folder `repoName` in cwd
     const chosenTemplate = templates.find(t => t.name === inputTemplate);
     if (isGithubPath) {
       console.log('\r');
       const spinner = createSpinner('Downloading files...\n').start();
-      await downloadDirectory(chosenTemplate.url, repoName);
+      await downloadDirectory(chosenTemplate.url, config.repoName, config);
       spinner.success({ text: `${ansi.green('Project created!')}\n` });
     } else {
       console.log('\r');
       const spinner = createSpinner('Copying files...\n').start();
-      await copyDirectory(chosenTemplate.path, repoName);
+      await copyDirectory(chosenTemplate.path, config.repoName, config);
       spinner.success({ text: `${ansi.green('Project created!')}\n` });
     }
 
-    // const { isGitRepo } = await inquirer.prompt({
-    //   type: 'confirm',
-    //   message: 'Initialize a git repository?',
-    //   name: 'isGitRepo',
-    //   default: false,
-    //   // default: true, // TODO: Check if possible to get from args
-    //   ...INQUIRER_DEFAULT_OPTS,
-    // });
+    // STEP 5 - initialize Git if specified in args already else start inquirer
+    let hasGitArg = argv.git;
+    if (!hasGitArg) {
+      const { isGitRepoInput } = await inquirer.prompt({
+        type: 'confirm',
+        message: 'Initialize a git repository?',
+        name: 'isGitRepoInput',
+        default: false,
+        ...INQUIRER_DEFAULT_OPTS,
+      });
+      hasGitArg = isGitRepoInput;
+    }
+    if (hasGitArg) await initializeGit(join(process.cwd(), config.repoName));
 
     process.exit(0);
   } catch (err) {
@@ -166,21 +119,3 @@ import { GITHUB_DIR_TYPE, GITHUB_FILE_TYPE, CONFIG_FILE_NAME, GITHUB_PATH_REGEX,
     process.exit(1);
   }
 })();
-
-async function getLocalTemplates(templatePath) {
-  const filesAndDirs = await fs.readdir(templatePath);
-  return filesAndDirs
-    .filter(fd => fs.lstatSync(templatePath + sep + fd).isDirectory())
-    .map(t => ({ name: t, path: templatePath + sep + t }));
-}
-
-async function callGitHubApi(token, url, raw = false) {
-  const headers = { accept: `application/vnd.github.${raw ? 'raw' : 'json'}` };
-  if (token) headers.Authorization = `token ${token}`;
-
-  const response = await fetch(url, { headers });
-  if (!response.ok) throw new Error(`Error calling GitHub API - ${response.statusText} | ${response.url}`);
-
-  if (raw) return await response.text();
-  return await response.json();
-}
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,92 @@
+import { resolve, join, sep } from 'pathe';
+import fetch from 'node-fetch';
+import fs from 'fs-extra';
+import { execa } from 'execa';
+import minimatch from 'minimatch';
+import ansi from 'ansi-colors';
+import { GITHUB_DIR_TYPE, GITHUB_FILE_TYPE } from './constants';
+
 export function escapeRegex(string) {
   return string.replace(/[.*+\-?^${}()[\]\\]/g, '\\$&');
+}
+
+export async function getLocalTemplates(templatePath) {
+  const filesAndDirs = await fs.readdir(templatePath);
+  return filesAndDirs
+    .filter(fd => fs.lstatSync(templatePath + sep + fd).isDirectory())
+    .map(t => ({ name: t, path: templatePath + sep + t }));
+}
+
+export async function callGitHubApi(url, token, raw = false) {
+  const headers = { accept: `application/vnd.github.${raw ? 'raw' : 'json'}` };
+  if (token) headers.Authorization = `token ${token}`;
+
+  const response = await fetch(url, { headers });
+  if (!response.ok) throw new Error(`Error calling GitHub API - ${response.statusText} | ${response.url}`);
+
+  if (raw) return await response.text();
+  return await response.json();
+}
+
+export async function downloadDirectory(url, targetDir, config) {
+  // create a folder if not exists
+  await fs.ensureDir(targetDir);
+  targetDir = resolve(targetDir);
+
+  const contents = await callGitHubApi(url, config.githubToken);
+
+  for (const content of contents) {
+    if (content.type === GITHUB_FILE_TYPE) {
+      const fileStr = await callGitHubApi(content.url, config.githubToken, true);
+      const filePathAbs = join(targetDir, content.name);
+      // replace slots in the file and write it
+      fs.writeFile(filePathAbs, replaceSlots(filePathAbs, fileStr, config));
+    } else if (content.type === GITHUB_DIR_TYPE) {
+      await downloadDirectory(content.url, join(targetDir, content.name), config);
+    } else {
+      console.log(ansi.red(`\n Unknown type for ${ansi.bold(content.name)}`));
+    }
+  }
+}
+
+export async function copyDirectory(path, targetDir, config) {
+  // create a folder if not exists
+  await fs.ensureDir(targetDir);
+  targetDir = resolve(targetDir);
+
+  const contents = await fs.readdir(path);
+
+  for (const content of contents) {
+    const contentAbsPath = path + sep + content;
+    if (fs.lstatSync(contentAbsPath).isFile()) {
+      const fileStr = await fs.readFile(contentAbsPath, 'utf8');
+      const filePathAbs = join(targetDir, content);
+      // replace slots in the file and write it
+      fs.writeFile(filePathAbs, replaceSlots(filePathAbs, fileStr, config));
+    } else if (fs.lstatSync(contentAbsPath).isDirectory()) {
+      await copyDirectory(contentAbsPath, join(targetDir, content), config);
+    } else {
+      console.log(ansi.red(`\n Unknown type for ${ansi.bold(content)}`));
+    }
+  }
+}
+
+function replaceSlots(filePath, content, config) {
+  if (config.slotPaths?.some(pattern => minimatch(filePath, pattern))) {
+    const { customSlots = {} } = config;
+    const slots = {
+      '[REPO_NAME]': config.repoName.trim(),
+      '[AUTHOR_NAME]': config.authorName.trim(),
+      ...customSlots,
+    };
+    const regex = new RegExp(escapeRegex(Object.keys(slots).join('|')), 'g');
+    return content.replace(regex, match => slots[match]);
+  }
+  return content;
+}
+
+export async function initializeGit(targetDir) {
+  const result = await execa('git', ['init'], { cwd: targetDir });
+  if (result.failed) return Promise.reject(new Error('Failed to initialize git'));
+  return true;
 }
