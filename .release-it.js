f50928bb36d7f7f1603babaf615815b72e8e4928
fa7ehak <fa7ehak@gmail.com>
2023-01-07 00:17:12 +0530

    chore: Update release-it config

--- .release-it.js
+++ .release-it.js
@@ -4,6 +4,7 @@ module.exports = {
     requireBranch: 'main',
     commitMessage: 'chore: Release v${version}',
     tagName: 'v${version}',
+    requireCommits: true, // require commits since last tag
     requireCleanWorkingDir: true, // exits if local not upto date with remote or if workdir is unclean
   },
   github: {
@@ -18,8 +19,8 @@ module.exports = {
     publish: true,
   },
   hooks: {
-    // check if there are commits since the last git tag, and runs lint
-    'before:init': ['if [ "$(git log $(git describe --tags --abbrev=0)..HEAD)" = "" ]; then exit 1; fi;', 'pnpm lint'],
+    // runs lint before releasing
+    'before:init': ['pnpm lint'],
     // build the package and generate a tarball for use in github releases
     'after:bump': 'pnpm build && pnpm tarball',
     'after:release': 'echo Successfully created a release v${version} for ${repo.repository}. Please add release notes and publish it!',
